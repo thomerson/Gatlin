76addb6333b32e3670ce92b8b00c904f08f333d3
thomerson <tanghongsheng123@126.com>
2020-03-07 11:51:17 +0800

    Update

---
+++ Thomerson.Gatlin.Model/UserPermission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Thomerson.Gatlin.Model
+{
+    public class UserPermission
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 请求Url
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
